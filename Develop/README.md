630795ec6df8c3c750483ee711ef67be2a2d5bda
Aden Neal <adenocbenji@gmail.com>
2024-07-17 14:17:27 -0500

    commit

---
+++ Develop/README.md
@@ -0,0 +1,41 @@
+# Readme generator
+## Description
+
+This project generates a readme file from user input in the terminal
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+go to my github page
+
+## Usage
+
+type "node index.js" and follow the promts
+
+## Contributing
+
+by cloning my code
+
+## Tests
+
+type "npm run test"
+
+
+## License
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## Questions
+
+For questions about the project, please contact [Aden Neal](mailto:Adenocbenji@gmail.com).
---
+++ Develop/index.js
@@ -0,0 +1,77 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+const questions = [ {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install your project? Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your project? Provide usage instructions:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How do you test your project? Provide testing instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'What is your name (or the name of the project author)?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Successfully generated ${fileName}`);
+        }
+      });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+      writeToFile('README.md', answers);
+    })
+    .catch(error => {
+      console.error('Error occurred during prompt:', error);
+    });
+}
+
+// Function call to initialize app
+init();
